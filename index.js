b3c9a5d2506cb252c47753a2b6de75f70e11a5c6
Josh Morris <jmo0624@gmail.com>
2020-08-30 20:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function testFunc() {
   return "hi"
 }
 
+function superbowlWin(superbowls){
+
+  let win = superbowls.find(superbowl => superbowl.result === "W")
+{	  return win ? win.year : undefined;
+	
+	}
